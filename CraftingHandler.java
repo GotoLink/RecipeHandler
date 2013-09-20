ee8a4a958f37c5551ff380b7f5bb0251a5b21f91
GotoLink <gotolinkminecraft@gmail.com>
2013-09-21 00:15:08 +0200

    Cleaning

--- CraftingHandler.java
+++ CraftingHandler.java
@@ -2,16 +2,14 @@ package assets.recipehandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCrafting;
-import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
-import cpw.mods.fml.common.ICraftingHandler;
 
 public class CraftingHandler {
 
