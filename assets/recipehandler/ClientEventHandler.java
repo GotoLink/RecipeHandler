14379d55795eb44bef828c538e9ab5615e2fe213
GotoLink <gotolinkminecraft@gmail.com>
2014-05-08 17:47:53 +0200

    Added support for modded crafting containers

--- assets/recipehandler/ClientEventHandler.java
+++ assets/recipehandler/ClientEventHandler.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.relauncher.Side;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerPlayer;
 import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraft.inventory.InventoryCrafting;
@@ -17,11 +18,17 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import org.lwjgl.input.Keyboard;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ClientEventHandler {
 	public static final Minecraft mc = Minecraft.getMinecraft();
     public static final KeyBinding key = new KeyBinding("RecipeSwitch", Keyboard.KEY_ADD, "key.categories.gui");
     public int recipeIndex;
     private ItemStack oldItem = null;
+    private HashMap<String, Field> knownCraftingContainer = new HashMap<String, Field>();
+    private HashSet<String> notCraftingContainer = new HashSet<String>();
 
     public ClientEventHandler() {
         ClientRegistry.registerKeyBinding(key);
@@ -29,15 +36,10 @@ public class ClientEventHandler {
 
 	@SubscribeEvent
 	public void onRenderGui(RenderGameOverlayEvent.Text event) {
-		if (mc != null && mc.thePlayer != null) {
+		if (mc.theWorld != null && mc.thePlayer != null) {
 			EntityClientPlayerMP player = mc.thePlayer;
 			if (player.openContainer != null) {
-				InventoryCrafting craft = null;
-				if (player.openContainer instanceof ContainerPlayer) {
-					craft = ((ContainerPlayer) player.openContainer).craftMatrix;
-				} else if (player.openContainer instanceof ContainerWorkbench) {
-					craft = ((ContainerWorkbench) player.openContainer).craftMatrix;
-				}
+				InventoryCrafting craft = getCraftingMatrix(player.openContainer);
 				if (craft != null) {
 					int result = CraftingHandler.getCraftResult(craft, mc.theWorld).size();
 					if (result > 1) {
@@ -56,15 +58,10 @@ public class ClientEventHandler {
     @SubscribeEvent
     public void keyDown(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.START && key.getIsKeyPressed()) {
-            if (mc!=null && mc.thePlayer != null) {
+            if (mc.theWorld != null && mc.thePlayer != null) {
                 EntityClientPlayerMP player = mc.thePlayer;
                 if (player.openContainer != null) {
-                    InventoryCrafting craft = null;
-                    if (player.openContainer instanceof ContainerPlayer) {
-                        craft = ((ContainerPlayer) player.openContainer).craftMatrix;
-                    } else if (player.openContainer instanceof ContainerWorkbench) {
-                        craft = ((ContainerWorkbench) player.openContainer).craftMatrix;
-                    }
+                    InventoryCrafting craft = getCraftingMatrix(player.openContainer);
                     if (craft != null) {
                         if (recipeIndex == Integer.MAX_VALUE) {
                             recipeIndex = 0;
@@ -80,5 +77,41 @@ public class ClientEventHandler {
                 }
             }
         }
+    }
+
+    private InventoryCrafting getCraftingMatrix(Container container){
+        if (container instanceof ContainerPlayer) {
+            return ((ContainerPlayer) container).craftMatrix;
+        } else if (container instanceof ContainerWorkbench) {
+            return ((ContainerWorkbench) container).craftMatrix;
+        }else {
+            String name = container.getClass().getName();
+            if (!notCraftingContainer.contains(name)) {
+                Field f = knownCraftingContainer.get(name);
+                if (f == null) {
+                    for (Field field : container.getClass().getDeclaredFields()) {
+                        if (field!=null && InventoryCrafting.class.isAssignableFrom(field.getClass())) {
+                            try {
+                                InventoryCrafting craft = InventoryCrafting.class.cast(field.get(container));
+                                if(craft!=null){
+                                    knownCraftingContainer.put(name, field);
+                                    return craft;
+                                }
+                            } catch (ReflectiveOperationException ref) {
+                                continue;
+                            }
+                        }
+                    }
+                    notCraftingContainer.add(name);
+                } else {
+                    try {
+                        return InventoryCrafting.class.cast(f.get(container));
+                    } catch (ReflectiveOperationException ref) {
+                        knownCraftingContainer.put(name, null);
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
