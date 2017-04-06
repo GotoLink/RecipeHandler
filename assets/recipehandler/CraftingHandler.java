22f12e3c532db242679260ad16cb23a1407a75e6
GotoLink <gotolinkminecraft@gmail.com>
2017-04-06 22:10:13 +0200

    More consistent update delay

--- assets/recipehandler/CraftingHandler.java
+++ assets/recipehandler/CraftingHandler.java
@@ -7,7 +7,6 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.util.*;
@@ -17,30 +16,45 @@ public final class CraftingHandler {
     private static HashSet<String> notCraftingContainer;
     private static Field slotCraftInv;
     private static int previousNumberOfCraft;
-    private static int delayTimer = 10;
+    private static long delayTimer = 0;
     private static int recipeIndex;
 
+    /**
+     * Enable guessing work over the crafting inventory space
+     */
     public static void enableGuessing(){
         knownCraftingContainer = new HashMap<String, Field>();
         notCraftingContainer = new HashSet<String>();
         slotCraftInv = ReflectionHelper.findField(SlotCrafting.class, "field_75239_a", "craftMatrix");
     }
 
+    /**
+     * The state of this helper
+     * @return The craft index
+     */
     public static int getRecipeIndex(){
         return recipeIndex;
     }
 
+    /**
+     * Apply new state to this helper
+     * @param id The craft index
+     */
     public static void setRecipeIndex(int id){
         if(id>=0){
             recipeIndex = id;
         }
     }
 
+    /**
+     * Get the crafting space within the given container
+     * May be guessing if enabled
+     * @param container The searched container
+     * @return The crafting space or null if none could be found
+     */
     @Nullable
     public static InventoryCrafting getCraftingMatrix(Container container){
-        if(container == null)
-            return null;
-        else if (container instanceof ContainerPlayer)
+        if (container instanceof ContainerPlayer)
             return ((ContainerPlayer) container).craftMatrix;
         else if (container instanceof ContainerWorkbench)
             return ((ContainerWorkbench) container).craftMatrix;
@@ -80,7 +94,12 @@ public final class CraftingHandler {
         return null;
     }
 
-    @Nonnull
+    /**
+     * Get the next craft result
+     * @param craft Crafting space
+     * @param world Where the craft happens
+     * @return The next result of the craft
+     */
     public static ItemStack findNextMatchingRecipe(InventoryCrafting craft, World world) {
         if (recipeIndex == Integer.MAX_VALUE) {
             recipeIndex = 0;
@@ -90,28 +109,45 @@ public final class CraftingHandler {
         return findMatchingRecipe(craft, world);
     }
 
-    @Nonnull
+    /**
+     * The current craft result
+     * Based on all the possible results, picked with the current state of this helper
+     * @param craft Crafting space
+     * @param world Where the craft happens
+     * @return The current result of the craft, EMPTY is none could be found
+     */
 	public static ItemStack findMatchingRecipe(InventoryCrafting craft, World world) {
 		if (!CraftingManager.getInstance().findMatchingRecipe(craft, world).isEmpty()) {
 			List<ItemStack> result = getCraftResult(craft, world);
-			if (result.size() == 0) {
+			if(previousNumberOfCraft!=result.size()) {
+                previousNumberOfCraft = result.size();
+                recipeIndex = 0;
+            }
+            delayTimer = world.getTotalWorldTime();
+			if (previousNumberOfCraft == 0) {
 				return ItemStack.EMPTY;
 			}
 			if (recipeIndex < 0) {
 				int j1 = -recipeIndex;
-				j1 %= result.size();
-				j1 = result.size() - j1;
-				if (j1 == result.size()) {
+				j1 %= previousNumberOfCraft;
+				j1 = previousNumberOfCraft - j1;
+				if (j1 == previousNumberOfCraft) {
 					j1 = 0;
 				}
 				return result.get(j1);
 			} else {
-				return result.get(recipeIndex % result.size());
+				return result.get(recipeIndex % previousNumberOfCraft);
 			}
 		}
 		return ItemStack.EMPTY;
 	}
 
+    /**
+     * Get all the results possible from the craft
+     * @param craft Crafting space
+     * @param world Where the craft happens
+     * @return List of all the craft results, empty is none could be found
+     */
 	public static List<ItemStack> getCraftResult(InventoryCrafting craft, World world) {
 		Iterator<?> recipes = CraftingManager.getInstance().getRecipeList().iterator();
 		ArrayList<ItemStack> arraylist = new ArrayList<ItemStack>();
@@ -124,11 +160,17 @@ public final class CraftingHandler {
 		return arraylist;
 	}
 
+    /**
+     * Get the crafting slot where the result should be displayed
+     * Can be guessed if the slot number isn't correct
+     * @param container Contains the craft space
+     * @param inventory Crafting space
+     * @param index Possible value for the slot number
+     * @return The crafting result slot, or null if none could be found
+     */
     @Nullable
     public static Slot getResultSlot(Container container, InventoryCrafting inventory, int index){
-        if(container == null)
-            return null;
-        else if(index < container.inventorySlots.size()){
+        if(index < container.inventorySlots.size()){
             Slot slot = container.getSlot(index);
             if(slot instanceof SlotCrafting)
                 return slot;
@@ -146,16 +188,22 @@ public final class CraftingHandler {
         return null;
     }
 
+    /**
+     * How many recipes apply in the current container
+     * Result may be a cached value
+     * @param container To craft into
+     * @param world Where the player crafts
+     * @return The number of recipes that can be crafted
+     */
     public static int getNumberOfCraft(Container container, World world){
-        if(delayTimer>20) {
-            delayTimer = 0;
+        if(world.getTotalWorldTime() - delayTimer > 10) {
+            delayTimer = world.getTotalWorldTime();
             InventoryCrafting craft = getCraftingMatrix(container);
             if (craft != null)
                 previousNumberOfCraft = getCraftResult(craft, world).size();
             else
                 previousNumberOfCraft = 0;
-        }else
-            delayTimer++;
+        }
         return previousNumberOfCraft;
     }
 }
