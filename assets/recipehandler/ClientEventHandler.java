a7c027f445b860581404b1a01958326b600e626a
GotoLink <gotolinkminecraft@gmail.com>
2018-02-10 15:39:53 +0100

    Fixed keybind handling Close #9

--- assets/recipehandler/ClientEventHandler.java
+++ assets/recipehandler/ClientEventHandler.java
@@ -2,23 +2,24 @@ package assets.recipehandler;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.*;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.translation.I18n;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+
+import javax.annotation.Nullable;
 
 public final class ClientEventHandler implements RecipeMod.IRegister{
     private KeyBinding key;
@@ -43,6 +44,7 @@ public final class ClientEventHandler implements RecipeMod.IRegister{
      *
      * @return the client player entity
      */
+    @Nullable
     @Override
     public EntityPlayer getPlayer(){
         return FMLClientHandler.instance().getClientPlayerEntity();
@@ -69,12 +71,10 @@ public final class ClientEventHandler implements RecipeMod.IRegister{
             GuiContainer screen = (GuiContainer) FMLClientHandler.instance().getClient().currentScreen;
             //Simulate left click
             ItemStack temp = FMLClientHandler.instance().getClient().playerController.windowClick(screen.inventorySlots.windowId, slot.slotNumber, 0, ClickType.QUICK_MOVE, getPlayer());
-            ItemStack result = CraftingHandler.findMatchingRecipe(crafting, getWorld());
-            if(!slot.getHasStack() && !result.isEmpty()){
-                if (ItemStack.areItemStacksEqual(result, temp)) {
-                    //Recipe still match, so bounce it back to server
-                    RecipeMod.NETWORK.sendToServer(new ChangePacket(slot.slotNumber, result, CraftingHandler.getRecipeIndex()).setShift().toProxy(Side.SERVER));
-                }
+            ItemStack result = CraftingHandler.findCraftResult(crafting, getWorld());
+            if(!slot.getHasStack() && !result.isEmpty() && temp.isEmpty()){
+                //Recipe still match, so bounce it back to server
+                RecipeMod.NETWORK.sendToServer(new ChangePacket(slot.slotNumber, result, CraftingHandler.getRecipeIndex()).setShift().toProxy(Side.SERVER));
             }
         }
     }
@@ -83,16 +83,9 @@ public final class ClientEventHandler implements RecipeMod.IRegister{
      *
      * @return The world of the client
      */
+    @Nullable
     public static World getWorld(){
         return FMLClientHandler.instance().getWorldClient();
-    }
-
-    /**
-     *
-     * @return The container the player has opened
-     */
-    private Container getContainer(){
-        return getPlayer().openContainer;
     }
 
     /**
@@ -104,49 +97,57 @@ public final class ClientEventHandler implements RecipeMod.IRegister{
 		if (RecipeMod.cornerText && getPlayer() != null && FMLClientHandler.instance().getClient().currentScreen != null) {
 			int result = CraftingHandler.getNumberOfCraft(getContainer(), getWorld());
 			if (result > 1) {
-				event.getRight().add(I18n.translateToLocalFormatted("handler.found.text", result));
+				event.getRight().add(I18n.format("handler.found.text", result));
 			}
 		}
 	}
 
     /**
-     * Client tick that happens after all keybinds are processed, in a loaded world
-     * @param event the tick event
+     * Keyboard event that happens after the keybind is processed in a screen
+     * @param event the keyboard event
      */
     @SubscribeEvent
-    public void keyDown(InputEvent.MouseInputEvent event) {
-        if (FMLClientHandler.instance().getClient().currentScreen != null) {
-            if(key != null) {//Switch keybind logic
-                if (GameSettings.isKeyDown(key)) {
-                    if (!pressed) {
-                        pressed = true;
-                        pressed();
-                    }
-                } else if (pressed)
-                    pressed = false;
-            }
-            if(Mouse.isButtonDown(0) && GuiScreen.isShiftKeyDown()){//Shift click
-                Slot result = null;
-                if(FMLClientHandler.instance().getClient().currentScreen instanceof GuiContainer){
-                    Slot slot = ((GuiContainer) FMLClientHandler.instance().getClient().currentScreen).getSlotUnderMouse();
-                    if(!(slot instanceof SlotCrafting))//Other inventory event, don't mess it up
-                        return;
-                    result = slot;
+    public void keyDown(GuiScreenEvent.KeyboardInputEvent.Post event) {
+        if(key != null && Keyboard.getEventKey() == key.getKeyCode() && getPlayer() != null) {//Switch keybind logic
+            if (Keyboard.getEventKeyState()) {
+                if (!pressed) {
+                    pressed = true;
+                    pressed();
                 }
-                InventoryCrafting craft = CraftingHandler.getCraftingMatrix(getContainer());
-                if(craft != null){
-                    if(result == null) {//Shouldn't happen, but who knows
-                        result = CraftingHandler.getResultSlot(getContainer(), craft, 0);
+            } else if (pressed)
+                pressed = false;
+        }
+    }
+
+    /**
+     * Mouse event that happens after the keybind is processed in a screen
+     * @param event the mouse event
+     */
+    @SubscribeEvent
+    public void mouseDown(GuiScreenEvent.MouseInputEvent.Post event) {
+        if(Mouse.getEventButton() == 0 && Mouse.getEventButtonState() && GuiScreen.isShiftKeyDown()){//Shift click
+            if(getPlayer() == null || CraftingHandler.getNumberOfCraft(getContainer(), getWorld()) < 2)//Not a conflict
+                return;
+            Slot result = null;
+            if(event.getGui() instanceof GuiContainer){
+                Slot slot = ((GuiContainer) event.getGui()).getSlotUnderMouse();
+                if(!(slot instanceof SlotCrafting))//Other inventory event, don't mess it up
+                    return;
+                result = slot;
+            }
+            InventoryCrafting craft = CraftingHandler.getCraftingMatrix(getContainer());
+            if(craft != null){
+                if(result == null) {//Shouldn't happen, but who knows
+                    result = CraftingHandler.getResultSlot(getContainer(), craft, 0);
+                }
+                if(result != null){
+                    ItemStack res = CraftingHandler.findCraftResult(craft, getWorld());
+                    if (res.isEmpty()){
+                        oldItem = ItemStack.EMPTY;
+                    } else if(!ItemStack.areItemStacksEqual(res, result.getStack())){
+                        RecipeMod.NETWORK.sendToServer(new ChangePacket(result.slotNumber, res, CraftingHandler.getRecipeIndex()).setShift().toProxy(Side.SERVER));
+                        oldItem = res;
                     }
-                    if(result != null){
-                        ItemStack res = CraftingHandler.findMatchingRecipe(craft, getWorld());
-                        if (res.isEmpty()){
-                            oldItem = ItemStack.EMPTY;
-                        } else if(!ItemStack.areItemStacksEqual(res, result.getStack())){
-                            RecipeMod.NETWORK.sendToServer(new ChangePacket(result.slotNumber, res, CraftingHandler.getRecipeIndex()).setShift().toProxy(Side.SERVER));
-                            oldItem = res;
-                        }
-					}
                 }
             }
         }
@@ -165,7 +166,7 @@ public final class ClientEventHandler implements RecipeMod.IRegister{
 			} else if (!ItemStack.areItemStacksEqual(res, oldItem)) {
 			    int index = 0;//The default craft result slot index for containers
                 Slot slot = CraftingHandler.getResultSlot(getContainer(), craft, index);
-                if(slot!= null)
+                if(slot != null)
                     index = slot.slotNumber;
                 RecipeMod.NETWORK.sendToServer(new ChangePacket(index, res, CraftingHandler.getRecipeIndex()).toProxy(Side.SERVER));
                 oldItem = res;
